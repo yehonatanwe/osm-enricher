997723d7829fd80891b4c40a8c19ebc30dfe275e
Yehonatan Weinberger <yehonatanwe@gmail.com>
2020-08-12 23:47:00 +0300

    update tests

--- tests/test_osm_fetcher.py
+++ tests/test_osm_fetcher.py
@@ -13,3 +13,9 @@ def test_fetch_osm_invalid_data():
     except Exception as e:
         assert osm_fetcher_exceptions.MISSING_FIELDS_ERROR in str(e)
 
+
+def test_fetch_osm_invalid_coordinates():
+    try:
+        fetch_osm_data(81, 180)
+    except Exception as e:
+        assert osm_fetcher_exceptions.FAILED_RETRIEVE_ERROR[:-2] in str(e)
